dcbb55fc7da7a7c714088e6974ac5c9d92007de9
chnaumangujjar0 <chnaumangujjar0@gmail.com>
2026-05-06 22:45:40 +0500

    just minor changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,34 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
-
+import {apiError} from "../utils/apiError.js"
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message : "ok"
-    })
+    // res.status(200).json({
+    //     message : "ok"
+    // })
+
+    // get data from frontend
+    // validationn - empty or not
+    //chech if user already exist
+    // check for images, avatar
+    // upload to cloudinary
+    // create user object - create db
+    // remove password and refresh token from field
+    // check for user creation
+    // return response
+    
+    const {email , username, password, fullName} = req.body;
+    console.log("email : ",email)
+
+    if (
+        [username, email , fullName, password].some((field)
+        => field?.trim() === "")
+    ) {
+        throw new apiError(400,"All fields are required")
+    }
+
+    if(!email.includes("@")){
+        throw new apiError(400,"Enter valid email")
+    }
+    
 } ) 
 
 export  {registerUser};
