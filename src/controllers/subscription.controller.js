4a4b9a99b9a55ff1e302be2ec3c3c5c26617b083
chnaumangujjar0 <chnaumangujjar0@gmail.com>
2026-05-29 06:38:09 +0500

    feat: enhance subscription functionality with user channel subscribers and subscribed channels retrieval

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { Subscription } from "../models/subscription.model.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
@@ -5,6 +6,10 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) =>{
     const {channelId} = req.params
+
+    if(!channelId){
+        throw new ApiError(400, "Invalid channel id")
+    }
     const existingSubscription = await Subscription.findOne({
         subscriber: req.user._id,
         channel: channelId
@@ -47,4 +52,116 @@ const toggleSubscription = asyncHandler(async (req, res) =>{
     )
 })
 
-export {toggleSubscription}
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+
+    if(!channelId){
+        throw new ApiError(400, "Subscriber id required in params")
+    }
+    const subscriber = await Subscription.aggregate([
+        {
+            $match: {
+                channel: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "subscriber",
+                foreignField: "_id",
+                pipeline: [
+                    {
+                        $project: {
+                        username: 1,
+                        fullName: 1,
+                        avatar: 1
+                        }
+                    }
+                ],
+                as: "subscriberInfo"
+            }
+        },
+        {
+            $unwind: "$subscriberInfo" // it convert the array data into object
+        },
+        {
+            $project: {
+                _id: 0,
+                subscriberId: "$subscriberInfo._id",
+                username: "$subscriberInfo.username",
+                email: "$subscriberInfo.email",
+                fullName: "$subscriberInfo.fullName",
+                avatar: "$subscriberInfo.avatar"
+            }
+        }
+    ])
+    if(!subscriber){
+        throw new ApiError(400,"Errpor while fethching subscribers data")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            subscriber,
+            "subscribers fetched successfully"
+        )
+    )
+})
+
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const { subscriberId } = req.params
+
+    if(!subscriberId){
+        throw new ApiError(400, "Subscriber id required in params")
+    }
+    const subscribedChannels = await Subscription.aggregate([
+        {
+            $match: {
+                subscriber: new mongoose.Types.ObjectId(subscriberId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "channel",
+                foreignField: "_id",
+                pipeline: [
+                    {
+                        $project: {
+                        username: 1,
+                        fullName: 1,
+                        avatar: 1
+                        }
+                    }
+                ],
+                as: "subscribedChannelInfo"
+            }
+        },
+        {
+            $unwind: "$subscribedChannelInfo"
+        },
+        {
+            $project: {
+               channelId: "$subscribedChannelInfo._id",
+                username: "$subscribedChannelInfo.username",
+                email: "$subscribedChannelInfo.email",
+                fullName: "$subscribedChannelInfo.fullName",
+                avatar: "$subscribedChannelInfo.avatar"
+            }
+        }
+    ])
+    
+    if(!subscribedChannels){
+        throw new ApiError(400,"Errpor while fethching subscribed channels data")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            subscribedChannels,
+            "fetch subscribed channels successfully!"
+        )
+    )
+})
+export {toggleSubscription, getUserChannelSubscribers, getSubscribedChannels}
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { toggleSubscription } from "../controllers/subscription.controller.js";
+import { getUserChannelSubscribers, toggleSubscription } from "../controllers/subscription.controller.js";
 
 
 const router = Router()
 
 router.route("/c/:channelId").post(verifyJwt, toggleSubscription)
+router.route("/c/:channelId").get(verifyJwt, getUserChannelSubscribers)
+router.route("/c/:channelId").get(verifyJwt, getUserChannelSubscribers)
 
 export default router;
